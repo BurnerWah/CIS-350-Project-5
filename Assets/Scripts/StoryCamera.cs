30089d8fd082e4b7786848dbe8730dc2dbfcd26e
GerardS0406 <48145351+GerardS0406@users.noreply.github.com>
2022-04-18 00:09:16 -0500

    Story Update

---
+++ Assets/Scripts/StoryCamera.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryCamera : MonoBehaviour
+{
+    [SerializeField] private GameObject ClickManager;
+    StoryClicker clicker;
+    int moment = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        clicker = ClickManager.GetComponent<StoryClicker>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(clicker.moment == 4 && moment != 4)
+        {
+            moment = 4;
+            gameObject.GetComponent<Camera>().orthographicSize = 18;
+            transform.position = new Vector3(transform.position.x, -7, transform.position.z);
+        }
+        if(clicker.moment == 6 && moment != 6)
+        {
+            moment = 6;
+            gameObject.GetComponent<Camera>().orthographicSize = 3;
+            transform.position = new Vector3(-6.1f, -3.5f, transform.position.z);
+        }
+    }
+}
