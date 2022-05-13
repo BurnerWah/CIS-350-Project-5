efe1a7835763ca104d8a3eef44aed033198061b1
GerardS0406 <48145351+GerardS0406@users.noreply.github.com>
2022-05-13 11:50:01 -0500

    fixes, health bar

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -6,16 +6,17 @@ public class HealthBar : MonoBehaviour
 {
     [SerializeField] BossCovid boss;
 
-    float scaleX_maxHealth = .975f;
+    float scaleX_maxHealth = .98f;
 
     void Start()
     {
-        UpdateHealth();
+        float newScaleX = (scaleX_maxHealth * (1));
+        transform.localScale = new Vector3(newScaleX, transform.localScale.y, 1);
     }
 
     public void UpdateHealth()
     {
-        float newScaleX = scaleX_maxHealth * boss.BossHealth / boss.BossMaxHealth;
+        float newScaleX = (scaleX_maxHealth * ((float)boss.BossHealth / (float)boss.BossMaxHealth));
         transform.localScale = new Vector3(newScaleX, transform.localScale.y, 1);
     }
 }
